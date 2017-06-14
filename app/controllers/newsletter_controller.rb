0541585fa2d5f0cfdede0d201d2c5d728eb47961
nonamme <lewandowskikamil@yahoo.co.uk>
2017-06-14 08:05:34 +0100

    Routes changes, menu and foot update

--- app/controllers/newsletter_controller.rb
+++ app/controllers/newsletter_controller.rb
@@ -4,18 +4,25 @@ class NewsletterController < ApplicationController
 
   def create
     @member = Newsletter.new(member_params)
-
     if @member.save
-      flash[:notice] = "Thank you. You successfully sign up to be our member."
-      redirect_to root_path
+      successfully_member_signed_up
     else
-      flash[:error] = "You can not sign up right now! Please try again later."
-      redirect_to root_path
+      failed_member_signed_up
     end
   end
 
   private
     def member_params
       params.require(:newsletter).permit(:email)
+    end
+
+    def successfully_member_signed_up
+      flash[:notice] = "Thank you. You successfully sign up to be our member."
+      redirect_to root_path
+    end
+
+    def failed_member_signed_up
+      flash[:error] = "You can not sign up right now! Please try again later."
+      redirect_to root_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
 
   root "users#index", to: "users/index"
 
+  resources :users
+  # get "users/new" => "users#new"
+  # get "users/create" => "users#create"
+
+  # post "users/new" => "users#new"
   post 'newsletter/create' => 'newsletter#create', as: 'create'
 end
