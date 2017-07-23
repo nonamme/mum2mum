4c5361b00d87b98526262bdf0aa0344d647c095f
nonamme <lewandowskikamil@yahoo.co.uk>
2017-07-23 05:45:16 +0100

    Paperclip gem and styling comments form
    Changes in gems and basic style for comment form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,15 @@
 class User < ApplicationRecord
   enum status: [:coffee, :shopping, :helpful]
 
-  has_many :images
   has_many :comments
 
   has_one :address
+  has_one :image
 
-  accepts_nested_attributes_for :images, :address
+  accepts_nested_attributes_for :image, :address
+
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   validates :name, presence: true, length: { minimum: 3 }
   validates_associated :address
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718092244) do
+ActiveRecord::Schema.define(version: 20170723041538) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "city"
@@ -58,6 +58,10 @@ ActiveRecord::Schema.define(version: 20170718092244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["id"], name: "index_users_on_id"
   end
 
