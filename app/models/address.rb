e8d506799b79b40127ab1f7caac1033874893ed8
nonamme <lewandowskikamil@yahoo.co.uk>
2017-08-07 04:28:16 +0100

    Validation changes

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,6 @@
 class Address < ApplicationRecord
-  geocoded_by :address
-  after_validation :geocode
-
   belongs_to :user, optional: true
+
   validates :city, presence: true, length: { minimum: 3 }
 
   def address
