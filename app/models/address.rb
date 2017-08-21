2376febaf36df3c1ba9c0ffcc857a499cb9980f9
nonamme <lewandowskikamil@yahoo.co.uk>
2017-08-21 04:35:51 +0100

    Basic validation for address

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,9 @@ class Address < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :city, presence: true, length: { minimum: 3 }
+  validates :home_number, numericality: true, {only_integer: true}
+  validates :street, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :post_code, format: { with: /\A[a-zA-Z0-9 \-]\z/}
 
   def address
     [home_number, street, city, country].compact.join(', ')
