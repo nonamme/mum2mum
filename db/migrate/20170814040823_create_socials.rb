c7f2caca658945f236ffd46c395646c2a71b5dde
nonamme <lewandowskikamil@yahoo.co.uk>
2017-08-22 06:07:25 +0100

    Add default value  for socials database

--- db/migrate/20170814040823_create_socials.rb
+++ db/migrate/20170814040823_create_socials.rb
@@ -1,9 +1,9 @@
 class CreateSocials < ActiveRecord::Migration[5.1]
   def change
     create_table :socials do |t|
-      t.string :facebook
-      t.string :twitter
-      t.string :instagram
+      t.string :facebook, default: "Not applicable"
+      t.string :twitter, default: "Not applicable"
+      t.string :instagram, default: "Not applicable"
 
       t.timestamps
     end
