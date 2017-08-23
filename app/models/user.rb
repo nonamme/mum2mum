b7eaafb5488ce715c26ed8ac9140d4e939c4c238
nonamme <lewandowskikamil@yahoo.co.uk>
2017-08-23 04:45:07 +0100

    Change order of methods in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_attached_file :image, :style => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => '/images/:style/avatar.png'
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
   enum status: [:coffee, :shopping, :helpful, :status]
 
   has_secure_password
@@ -9,9 +12,6 @@ class User < ApplicationRecord
   has_one :address
 
   accepts_nested_attributes_for :address
-
-  has_attached_file :image, :style => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => '/images/:style/avatar.png'
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   validates :name, presence: true, length: { minimum: 3 }
   validates_associated :address
