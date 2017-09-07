92efc6f163a152281afd99c567132d978fd38799
nonamme <lewandowskikamil@yahoo.co.uk>
2017-09-07 06:20:44 +0100

    Add comment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Setting for AWS 
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
